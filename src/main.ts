9608a887ce13c0e64dc04c3a90cd34b4080806b9
sabinbaniya <dummymail@tutanota.de>
2024-02-27 17:37:28 +0545

    fix: update cookie domain 3

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        domain: '*.ondigitalocean.app',
+        domain: 'ondigitalocean.app',
         httpOnly: true,
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
